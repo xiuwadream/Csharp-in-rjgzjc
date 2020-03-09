f2e513341502a6df910218e552387b1782f73eb3
alaoks <493088943@qq.com>
2020-03-09 10:21:05 +0800

    add homework4

---
+++ Homework4/Clock/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework4._2
+{
+    delegate void ClockEventHandler(Object sender, ClockArgs arg);
+    class ClockArgs
+    {
+        private int time;
+        public ClockArgs()
+        {
+            this.Time = 0;
+        }
+
+        public int Time { get => time; set => time = value; }
+
+        public void NextTime()
+        {
+            Time += 1;
+        }
+    }
+    class Clock
+    {
+        public event ClockEventHandler Tick;
+        public event ClockEventHandler Alarm;
+
+        private readonly int times;//定时的时间限制
+        public Clock(int times)
+        {
+            this.times = times;
+            Tick += (Object sender, ClockArgs arg) => { };
+            Alarm += (Object sender, ClockArgs arg) => { };
+        }
+        public void Start()
+        {
+            Console.WriteLine("启动时间：" + DateTime.Now);
+            ClockArgs c = new ClockArgs();
+            while (c.Time < times)
+            {
+                c.NextTime();
+                Thread.Sleep(1000);
+                Tick(this, c);              
+            }
+            Alarm(this, c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock(SetTime());
+            myClock.Tick += (Object sender, ClockArgs arg) =>
+             {
+                 Console.WriteLine($"滴答一下:第{arg.Time}秒");
+             };
+            myClock.Alarm += (Object sender, ClockArgs arg) =>
+             {
+                 Console.WriteLine($"时间到,共{arg.Time}秒");
+             };
+            myClock.Alarm += (Object sender, ClockArgs arg) =>
+             {
+                 //测试多播
+                 Console.WriteLine("当前系统时间：" + DateTime.Now);
+             };
+             myClock.Start();
+        }
+        static int SetTime()
+        {
+            Console.WriteLine("请设置定时时长(秒)：");
+
+            int t;
+            while (!int.TryParse(Console.ReadLine(),out t))
+            {
+                Console.WriteLine("不是合法时间，请重新输入");
+            }
+            return t;
+        }
+    }
+}
---
+++ Homework4/List/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericApplication;
+
+namespace Homework4_1
+{
+    class Program
+    {
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get => head;
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void Foreach(Action<T> act) {
+                Node<T> temp;
+                temp = head;
+                while (temp != null)
+                {
+                    act(temp.Data);
+                    temp = temp.Next;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int sum=0, max=int.MinValue, min=int.MaxValue;
+            GenericList<int> intlist = new GenericList<int>();
+            Random rd = new Random();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(1+rd.Next(10));
+            }
+            intlist.Foreach(x => Console.Write(x+" "));
+            Console.WriteLine();
+            intlist.Foreach(x => sum += x);
+            Console.WriteLine("sum:"+sum);
+            intlist.Foreach(x => max = x > max ? x : max);
+            intlist.Foreach(x => min = x < min ? x : min);
+            Console.WriteLine("max&min:"+max+" "+min);
+        }
+    }
+}
