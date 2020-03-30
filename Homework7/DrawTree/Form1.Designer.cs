8c6176e44bdafbfa4f8a37577d60af91f97ef7bb
alaoks <493088943@qq.com>
2020-03-30 10:48:29 +0800

    add homwork7

---
+++ Homework7/DrawTree/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Homework7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_draw = new System.Windows.Forms.Button();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_draw
+            // 
+            this.button_draw.Location = new System.Drawing.Point(0, 382);
+            this.button_draw.Name = "button_draw";
+            this.button_draw.Size = new System.Drawing.Size(207, 23);
+            this.button_draw.TabIndex = 0;
+            this.button_draw.Text = "Draw";
+            this.button_draw.UseVisualStyleBackColor = true;
+            this.button_draw.Click += new System.EventHandler(this.button_draw_Click);
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.Location = new System.Drawing.Point(76, 10);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(100, 25);
+            this.txtDepth.TabIndex = 1;
+            this.txtDepth.Text = "10";
+            this.txtDepth.Leave += new System.EventHandler(this.txtDepth_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "递归深度";
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDraw.Location = new System.Drawing.Point(12, 12);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(571, 422);
+            this.panelDraw.TabIndex = 3;
+            this.panelDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(70, 41);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(100, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "主干长度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "画笔颜色";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "选择颜色";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "右支比例";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "左支比例";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(70, 108);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(106, 56);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.Value = 6;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(70, 139);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(106, 56);
+            this.trackBar3.TabIndex = 12;
+            this.trackBar3.Value = 7;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(176, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "0.6";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(176, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0.7";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "15",
+            "30",
+            "45",
+            "60",
+            "75",
+            "90"});
+            this.comboBox1.Location = new System.Drawing.Point(76, 188);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 23);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.Text = "30";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "15",
+            "30",
+            "45",
+            "60",
+            "75",
+            "90"});
+            this.comboBox2.Location = new System.Drawing.Point(76, 229);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 23);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.Text = "45";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "右支角度";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 237);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 15);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "左支角度";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.button_draw);
+            this.panel1.Controls.Add(this.txtDepth);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Location = new System.Drawing.Point(589, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 423);
+            this.panel1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 447);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_draw;
+        private System.Windows.Forms.TextBox txtDepth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Homework7/DrawTree/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Homework7
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        const double PI = Math.PI;
+        double th1 = 45 * PI / 180;
+        double th2 = 30 * PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        int depth = 10;
+        int mainLength = 100;
+        Color color = Color.Pink;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        void DrawTree(int n, double x0, double y0, double length, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + length * Math.Cos(th);
+            double y1 = y0 + length * Math.Sin(th);
+
+            graphics.DrawLine(new Pen(color, n/2), (int)x0, (int)y0, (int)x1, (int)y1);
+
+            DrawTree(n - 1, x1, y1, length * per1, th + th1);
+            DrawTree(n - 1, x1, y1, length * per2, th - th2);
+        }
+
+        private void button_draw_Click(object sender, EventArgs e)
+        {
+                        
+            this.panelDraw.Invalidate();
+            
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            graphics = e.Graphics;
+            int px = this.panelDraw.Location.X + this.panelDraw.Width/2;
+            int py = this.panelDraw.Height;
+            DrawTree(depth, px, py, mainLength, -PI / 2);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.label3.Text = this.trackBar1.Value.ToString();
+            mainLength = this.trackBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.colorDialog1.ShowDialog();
+            color=this.colorDialog1.Color;
+            this.button1.BackColor = color;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.button1.BackColor = color;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.label7.Text = this.trackBar2.Value * 0.1 + "";
+            per1 = this.trackBar2.Value * 0.1;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            this.label8.Text = this.trackBar3.Value * 0.1 + "";
+            per2 = this.trackBar3.Value * 0.1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.th1 = Convert.ToInt32(this.comboBox1.SelectedItem.ToString()) * PI / 180;                
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.th2 = Convert.ToInt32(this.comboBox2.SelectedItem.ToString()) * PI / 180;
+        }
+
+        private void txtDepth_Leave(object sender, EventArgs e)
+        {
+            if (!int.TryParse(this.txtDepth.Text, out depth))
+            {
+                MessageBox.Show("请输入一个整数");
+            }
+        }
+    }
+
+    
+}
